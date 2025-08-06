cba724243fda6b983f86ce75e9ebea10307ee926
ANNGLORIOUS <mueniannglorious@gmail.com>
2025-08-06 09:06:50 +0300

    created the client serializer for checking their details, list enabled to create and updated the client

--- Backend/Lumora/myapp/mysite/serializer.py
+++ Backend/Lumora/myapp/mysite/serializer.py
@@ -2,8 +2,8 @@
 "conver between django models and json for Api responses"
 from rest_framework import serializers
 from django.contrib.auth import authenticate
-from django.contrib.auth.password_validation import validate_password
-from .models import User
+from django.contrib.auth.password_validation import validate_password, validated_data
+from .models import User,Client, ClientContact
 
 class UserSerializer(serializers.ModelSerializer):
     is_verified = serializers.BooleanField(source="profile.is_verified", read_only=True)
@@ -80,3 +80,79 @@ class LoginSerializer(serializers.Serializer):
             raise serializers.ValidationError(
                 'Email and password are required.'
             )
+        
+class TimestampedSerializer(serializers.ModelSerializer):
+    """Base serializer with timestamp fields"""
+    
+    created_at = serializers.DateTimeField(read_only=True)
+    updated_at = serializers.DateTimeField(read_only=True)
+
+
+class TenantFilteredSerializer(TimestampedSerializer):
+    """Base serializer that filters by tenant"""
+    
+    def create(self, validated_data):
+        # Automatically add tenant from request
+        validated_data['tenant'] = self.context['request'].user.current_tenant
+        return super().create(validated_data)
+
+
+class ClientContactSerializer(serializers.ModelSerializer):
+    "client contact serializer"
+    class Meta:
+        model = ClientContact
+        field = ['id','name','email','phone','position','is_primary','created_at']
+        read_only_fields = ['id','created_at']
+
+class ClientListSerializer(TenantFilteredSerializer):
+    "client list serializer with basic information"
+
+    total_projects = serializers.ReadOnlyField()
+    active_projects = serializers.ReadOnlyField()
+    total_invoiced = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Client
+        fields = [
+            'id','name','email','company','status','total_projects',
+            'active_projects', 'total_invoiced','created_at','updated_at'
+        ]
+        read_only_fileds = ['id','created_at','updated_at']
+
+class ClientDetailSerializer(TenantFilteredSerializer):
+    "client detail serializer with full information"
+
+    contacts = ClientContactSerializer(many=True, read_only=True)
+    created_by = UserSerializer(read_only=True)
+    total_projects = serializers.ReadOnlyField()
+    active_projects = serializers.ReadOnlyField()
+    total_invoiced = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Client
+        field = [
+            'id', 'name', 'email', 'phone', 'company', 'address',
+            'website', 'tax_id', 'status', 'notes', 'contacts',
+            'created_by', 'total_projects', 'active_projects',
+            'total_invoiced', 'created_at', 'updated_at'
+        ]
+        read_only_fields = ['id', 'created_by', 'created_at', 'updated_at']
+
+        def create(self, validated_data):
+            validated_data['created_by'] = self.context['request'].user
+            return super().create(validated_data)
+        
+
+class ClientCreateUpdateSerializer(TenantFilteredSerializer):
+    "Client create/update serializer"
+
+    class Meta:
+        model = Client
+        fields = [
+            'name', 'email', 'phone', 'company', 'address',
+            'website', 'tax_id', 'status', 'notes'
+        ]
+
+    def create(self, validated_data):
+        validated_data['created_by'] = self.context['request'].user
+        return super().create(validated_data)
